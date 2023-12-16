5cd9ea03afabd1180804d1fd19f0131033891385
vhphuc <phucvh@avadagroup.com>
2023-12-16 09:16:13 +0700

    feat: add default setting for the init

---
+++ packages/functions/src/const/setting/defaulSetting.js
@@ -0,0 +1,12 @@
+export const defaultSettings = {
+  position: 'bottom-left',
+  hideTimeAgo: false,
+  truncateProductName: true,
+  displayDuration: 5,
+  firstDelay: 10,
+  popsInterval: 2,
+  maxPopsDisplay: 20,
+  excludedUrls: '',
+  includedUrls: '',
+  allowShow: 'all'
+};
